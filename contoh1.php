1a8f3163742504db572d77976ea2c487e76c5ab6
antoniwoo <jeremyantonio957@gmail.com>
2022-03-21 21:50:34 +0700

    menambahkan Welcome.php,contoh1.php

---
+++ contoh1.php
@@ -0,0 +1,9 @@
+<?php 
+class Contoh1 extends CI_Controller 
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Jeremy Antoni saya tinggal di tangerang olahraga yang saya sukai adalah Futsal";
+    }
+}
